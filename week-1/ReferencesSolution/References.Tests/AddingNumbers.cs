aba2f0f46c9819568f58bd25f63dea92e76ace3c
Vishi Pallerla <vishwas_pallerla@progressive.com>
2025-07-07 20:30:16 +0000

    end of day 1

---
+++ week-1/ReferencesSolution/References.Tests/AddingNumbers.cs
@@ -0,0 +1,22 @@
+﻿
+namespace References.Tests;
+
+public class AddingNumbers
+{
+    [Fact]
+    public void CanAddTwoNumbers()
+    {
+        // Given
+        int a = 10;
+        int b = 20;
+        int answer;
+
+        // When
+        answer = a + b;
+
+        // Then
+        // Answer should be 30
+        Assert.Equal(30, answer);
+        // This is a simple example, even though it's stupid
+    }
+}

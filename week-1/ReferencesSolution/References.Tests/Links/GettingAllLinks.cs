d112159c42152bc3f3095b14a93a071a631f0c0d
Vishi Pallerla <vishwas_pallerla@progressive.com>
2025-07-08 20:02:30 +0000

    updating day 2

---
+++ week-1/ReferencesSolution/References.Tests/Links/GettingAllLinks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace References.Tests.Links;
+
+internal class GettingAllLinks
+{
+    [Fact]
+    public async Task GettingLinksReturnsA200Ok()
+    {
+        // GET /links
+        var client = new HttpClient();
+        client.BaseAddress = new Uri("http://localhost:1337");
+
+        var response = await client.GetAsync("/links");
+        Assert.NotNull(response);
+
+        Assert.Equal(System.Net.Http.HttpStatusCode.OK, response.StatusCode);
+    }
+}

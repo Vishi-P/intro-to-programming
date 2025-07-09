cd3e48b8160393bd6c78a43c3a1f47c074d6c112
Vishi Pallerla <vishwas_pallerla@progressive.com>
2025-07-09 20:53:56 +0000

    end of day 3

---
+++ week-1/StringCalculatorKata-main/StringCalculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        // Used for Steps 1 and 2, commented out during Step 3
+        //return numbers == "" ? 0 : int.Parse(numbers);
+        int length = numbers.Length;
+        if (length == 0) return 0;
+        else if (length == 1) return int.Parse(numbers);
+        // Used for Step 3, commented out during Step 4
+        //else if (length == 3)
+        //{
+        //    string[] arr = numbers.Split(",");
+        //    return int.Parse(arr[0]) + int.Parse(arr[1]);
+        //}
+        else
+        {
+            int sum = 0;
+            char[] delimiters = [',', '\n', '#'];
+            string[] arr = numbers.Split(delimiters);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += int.Parse(arr[i]);
+            }
+            return sum;
+        }
+    }
+}
---
+++ week-1/StringCalculatorKata-main/StringCalculator/CalculatorTests.cs
@@ -0,0 +1,76 @@
+﻿
+
+using NSubstitute.Routing.Handlers;
+
+namespace StringCalculator;
+public class CalculatorTests
+{
+    // Number 1
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    // Number 2
+    [Theory]
+    [InlineData("2", 2)]
+    [InlineData("3", 3)]
+    [InlineData("4", 4)]
+    public void StringWithSingleInteger(string value, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(value);
+        Assert.Equal(expected, result);
+    }
+
+    // Number 3
+    [Theory]
+    [InlineData("2,2", 4)]
+    [InlineData("2,3", 5)]
+    [InlineData("4,4", 8)]
+    public void StringWithTwoIntegers(string values, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(values);
+        Assert.Equal(expected, result);
+    }
+
+    // Number 4
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("1,2,3", 6)]
+    [InlineData("1,2,3,4,5,6,7,8,9", 45)]
+    public void StringWithArbitraryLength(string values, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(values);
+        Assert.Equal(expected, result);
+    }
+
+    // Number 5
+    [Theory]
+    [InlineData("1\n2", 3)]
+	[InlineData("1\n2,3",6)]
+    public void MixDelimters(string values, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(values);
+        Assert.Equal(expected, result);
+    }
+
+    // Number 6
+    [Theory]
+    [InlineData("//#\\n1#2#3", 6)]
+    [InlineData("//#\n1#2,3\n1", 7)]
+    public void CustomDelimiters(string values, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(values);
+        Assert.Equal(expected, result);
+    }
+}
